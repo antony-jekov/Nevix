7e68dff634bcf544a3827143e3d0a1eb0fc093f9
Antony Jekov <antony.jekov@gmail.com>
2014-01-02 19:18:30 +0200

    Started a simple local db.

---
+++ Source/Jekov.Nevix/Desktop/Jekov.Nevix.Desktop.Common/Models/UserConfigurationsModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Jekov.Nevix.Desktop.Common.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class UserConfigurationsModel
+    {
+        public string SessionKey { get; set; }
+
+        public string BsPlayerLocation { get; set; }
+
+        public DateTime LastMediaUpdate { get; set; }
+    }
+}
---
+++ Source/Jekov.Nevix/Desktop/Jekov.Nevix.Desktop.Common/NevixLocalDbContext.cs
@@ -0,0 +1,29 @@
+﻿namespace Jekov.Nevix.Desktop.Common
+{
+    using Jekov.Nevix.Desktop.Common.Models;
+    using Newtonsoft.Json;
+    using System;
+    using System.IO;
+
+    public class NevixLocalDbContext
+    {
+        private UserConfigurationsModel configFile;
+        private const string ConfigFileName = "NevixConfig.dat";
+        private string configFilePath;
+
+        public NevixLocalDbContext()
+        {
+            string personalFolder = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            configFilePath = string.Format("{0}\\{1}", personalFolder, ConfigFileName);
+
+            if (File.Exists(configFilePath))
+            {
+                configFile = JsonConvert.DeserializeObject<UserConfigurationsModel>(File.ReadAllText(configFilePath));
+            }
+            else
+            {
+                File.WriteAllText(configFilePath, JsonConvert.SerializeObject(new UserConfigurationsModel()));
+            }
+        }
+    }
+}
