2ea8df24d685a1ed74f70ab413db78ef65435ea5
Antony Jekov <antony.jekov@gmail.com>
2014-01-03 09:37:13 +0200

    Added pusher for pubnub actions.

--- Source/Android/Nevix/nevix/src/main/java/com/antonyjekov/nevix/common/PusherManager.java
+++ Source/Android/Nevix/nevix/src/main/java/com/antonyjekov/nevix/common/PusherManager.java
@@ -1,7 +1,22 @@
 package com.antonyjekov.nevix.common;
 
-/**
- * Created by Antony Jekov on 1/3/14.
- */
+import com.pubnub.api.Pubnub;
+
+import org.json.JSONObject;
+
 public class PusherManager {
+    private final String SUBSCRIBE_KEY = "sub-c-c7a22dee-6f0c-11e3-9291-02ee2ddab7fe";
+    private final String PUBLISH_KEY = "pub-c-2db685fb-40f0-4f91-a074-31ab9993d2d6";
+
+    private Pubnub pusher;
+
+    public PusherManager(String channelName) {
+        pusher = new Pubnub(PUBLISH_KEY, SUBSCRIBE_KEY, false);
+    }
+
+    public void SendCommand(String cmd)
+    {
+
+        //pusher.publish();
+    }
 }
