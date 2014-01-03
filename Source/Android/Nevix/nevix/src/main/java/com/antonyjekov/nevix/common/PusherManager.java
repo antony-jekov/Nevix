4c0ebc88464e68edd5effa6483f77ad9fe6e47db
Antony Jekov <antony.jekov@gmail.com>
2014-01-03 10:32:43 +0200

    Done notifications pusher.

--- Source/Android/Nevix/nevix/src/main/java/com/antonyjekov/nevix/common/PusherManager.java
+++ Source/Android/Nevix/nevix/src/main/java/com/antonyjekov/nevix/common/PusherManager.java
@@ -1,21 +1,35 @@
 package com.antonyjekov.nevix.common;
 
+import android.util.Log;
+
+import com.pubnub.api.Callback;
 import com.pubnub.api.Pubnub;
-import com.google.gson.Gson;
+import com.pubnub.api.PubnubError;
 
 public class PusherManager {
     private final String SUBSCRIBE_KEY = "sub-c-c7a22dee-6f0c-11e3-9291-02ee2ddab7fe";
     private final String PUBLISH_KEY = "pub-c-2db685fb-40f0-4f91-a074-31ab9993d2d6";
 
     private Pubnub pusher;
+    private String channel;
+    private Callback callback;
 
     public PusherManager(String channelName) {
+        this.channel = channelName;
         pusher = new Pubnub(PUBLISH_KEY, SUBSCRIBE_KEY, false);
+
+        callback = new Callback() {
+            public void successCallback(String channel, Object response) {
+                Log.d("PUBNUB",response.toString());
+            }
+            public void errorCallback(String channel, PubnubError error) {
+                Log.d("PUBNUB", error.toString());
+            }
+        };
     }
 
     public void SendCommand(String cmd)
     {
-        //new Gson().toJson();
-        //pusher.publish();
+        pusher.publish(channel, cmd, callback );
     }
 }
