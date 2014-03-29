8cbd13dea1c597e6d0c7cb7887ed83f9aa498e1c
Antony Jekov <antony.jekov@gmail.com>
2014-03-29 22:29:05 +0200

    Forgot a file

---
+++ Source/Jekov.Nevix/Desktop/Jekov.Nevix.Desktop.Common/Players/Player.cs
@@ -0,0 +1,72 @@
+﻿namespace Jekov.Nevix.Desktop.Common.Players
+{
+    using Jekov.Nevix.Desktop.Common.Contracts;
+    using System;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Runtime.InteropServices;
+    using System.Threading;
+    using WindowsInput;
+    using WindowsInput.Native;
+
+    public abstract class Player : SystemPlayer
+    {
+        [DllImport("user32.dll")]
+        public static extern int SetForegroundWindow(IntPtr handleWindow);
+        
+        private Process playerProcess;
+
+        protected string PlayerLocation { get; set; }
+
+        protected string PlayerProcessName { get; set; }
+
+        protected Process PlayerProcess
+        {
+            get
+            {
+                if (playerProcess == null)
+                    playerProcess = Process.GetProcessesByName(PlayerProcessName).FirstOrDefault();
+                
+                if (playerProcess == null)
+                    playerProcess = Process.Start(PlayerLocation);
+
+                try
+                {
+                    if (playerProcess.HasExited)
+                        playerProcess.Start();
+                }
+                catch (Exception)
+                {
+                    playerProcess = null;
+                    return PlayerProcess;
+                }
+
+                return playerProcess;
+            }
+            set
+            {
+                playerProcess = value;
+            }
+        }
+
+        public Player(string playerProcessName, string playerLocation)
+        {
+            PlayerLocation = playerLocation;
+            PlayerProcessName = playerProcessName;
+        }
+
+        public override void OpenFile(string location)
+        {
+            if (!string.IsNullOrEmpty(PlayerLocation))
+            {
+                if(!PlayerProcess.HasExited)
+                    PlayerProcess.Kill();
+                PlayerProcess = Process.Start(PlayerLocation, location);
+            }
+            else
+            {
+                base.OpenFile(location);
+            }
+        }
+    }
+}
