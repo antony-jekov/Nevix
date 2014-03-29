f8a443517efa4833eb3bd7c4798f4dcc0877b222
Antony Jekov <antony.jekov@gmail.com>
2014-03-29 22:28:37 +0200

    Dropped the java desktop client.

--- Source/NevixDesktopClient/src/com/jekov/nevix/common/CallBack.java
+++
@@ -1,8 +0,0 @@
-package com.jekov.nevix.common;
-
-/**
- * Created by Antony Jekov on 3/28/2014.
- */
-public interface CallBack {
-    void onResult(String result);
-}
--- Source/NevixDesktopClient/src/com/jekov/nevix/common/Listener.java
+++
@@ -1,7 +0,0 @@
-package com.jekov.nevix.common;
-
-/**
- * Created by Antony Jekov on 3/28/2014.
- */
-public class Listener {
-}
