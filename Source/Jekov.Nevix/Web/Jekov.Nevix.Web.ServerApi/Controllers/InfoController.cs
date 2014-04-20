9fdf41bf96ccd11912bc632637f522e257846200
Antony Jekov <antony.jekov@gmail.com>
2014-04-20 17:48:15 +0300

    Added files

---
+++ Source/Jekov.Nevix/Web/Jekov.Nevix.Web.ServerApi/Controllers/InfoController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Jekov.Nevix.Web.ServerApi.Controllers
+{
+    public class InfoController : Controller
+    {
+        //[OutputCache(CacheProfile = "Cache1Hour")]
+        public ActionResult GettingStarted()
+        {
+            return View();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
